802db367f75d4f58da601fb3e94a2734ce84cf3e
notme84 <nguster@gmail.com>
2024-05-25 01:23:47 -0700

    Basic enemy movement system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,41 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingLeft = 0.5f;
+    [SerializeField] float paddingRight = 0.5f;
+    [SerializeField] float paddingTop = 0;
+    [SerializeField] float paddingBottom = 0;
+
     Vector2 rawInput;
-    float moveSpeed = 10f;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start () 
+    {
+        InitBounds();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+    
     void Update()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, 
+            minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y,
+            minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
     }
 }
---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWavepoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
